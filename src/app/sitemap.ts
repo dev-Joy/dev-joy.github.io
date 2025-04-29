456e8c2ab302923ab1d68e022f457a1b040e6024
Joy <kjuhee.dev@gmail.com>
2025-04-30 00:57:11 +0900

    [fix] sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,15 @@
-import { getSitemapPostList } from "@/service/posts";
-import { MetadataRoute } from "next";
+import { getSitemapPostList } from '@/service/posts';
+import { MetadataRoute } from 'next';
+export const dynamic = 'force-static';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-	const postList = await getSitemapPostList();
-	const baseUrl = "https://dev-joy.github.io";
-	return [
-		{
-			url: baseUrl,
-			lastModified: new Date(),
-		},
-		...postList,
-	];
+  const postList = await getSitemapPostList();
+  const baseUrl = 'https://dev-joy.github.io';
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    ...postList,
+  ];
 }
