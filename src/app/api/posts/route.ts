47401bbf625cd46ec8e36a2b1535e08c4ac0a983
Joy <kjuhee.dev@gmail.com>
2025-05-07 15:11:49 +0900

    [mod] api ISR

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,6 +1,10 @@
 import { getSortedPostList } from '@/service/posts';
 
+export const revalidate = 60;
+
 export async function GET() {
   const posts = await getSortedPostList();
-  return new Response(JSON.stringify(posts));
+  return new Response(JSON.stringify(posts), {
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
