923c3b288333d3634988200fcf76c8442c3fa1df
Joy <kjuhee.dev@gmail.com>
2025-05-27 14:37:03 +0900

    [mod] category

--- src/components/post_list/CategoryList.tsx
+++ src/components/post_list/CategoryList.tsx
@@ -1,47 +1,75 @@
-"use client";
+'use client';
 
-import { Category } from "@/service/types";
+import { Category } from '@/service/types';
 import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from "../ui/select";
-import { useRouter } from "next/navigation";
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select';
+import { useRouter } from 'next/navigation';
+import { CategoryButton } from './CategoryButton';
 
 type Props = {
-	allPostCount: number;
-	categoryList: Category[];
-	selectedCategory?: string;
+  allPostCount: number;
+  categoryList: Category[];
+  selectedCategory?: string;
 };
 
 export default function CategoryList({
-	allPostCount,
-	categoryList,
-	selectedCategory = "all",
+  allPostCount,
+  categoryList,
+  selectedCategory = 'all',
 }: Props) {
-	const router = useRouter();
-	const onCategoryChange = (category: string) => {
-		const url = category === "all" ? "/posts" : `/posts/${category}`;
-		router.push(url);
-	};
+  const router = useRouter();
+  const onCategoryChange = (category: string) => {
+    const url = category === 'all' ? '/posts' : `/posts/${category}`;
+    router.push(url);
+  };
 
-	return (
-		<section className='mb-10'>
-			<Select onValueChange={onCategoryChange} defaultValue={selectedCategory}>
-				<SelectTrigger className='w-[180px]'>
-					<SelectValue placeholder='Theme' />
-				</SelectTrigger>
-				<SelectContent>
-					<SelectItem value='all'>All ({allPostCount})</SelectItem>
-					{categoryList.map(({ category, count }) => (
-						<SelectItem key={category} value={category}>
-							{category} ({count})
-						</SelectItem>
-					))}
-				</SelectContent>
-			</Select>
-		</section>
-	);
+  return (
+    <>
+      <section className='mb-10 hidden sm:block'>
+        <ul className='flex gap-3'>
+          <CategoryButton
+            href='/blog'
+            isCurrent={selectedCategory === 'all'}
+            displayName='All'
+            count={allPostCount}
+          />
+          {categoryList.map(({ category, count }) => (
+            <CategoryButton
+              key={category}
+              href={`/${category}`}
+              displayName={category}
+              isCurrent={category === selectedCategory}
+              count={count}
+            />
+          ))}
+        </ul>
+      </section>
+      <section className='mb-10 sm:hidden'>
+        <Select
+          onValueChange={onCategoryChange}
+          defaultValue={selectedCategory}
+        >
+          <SelectTrigger className='w-[180px]'>
+            <SelectValue placeholder='Theme' />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value='all'>All ({allPostCount})</SelectItem>
+            {categoryList.map(({ category, count }) => (
+              <SelectItem
+                key={category}
+                value={category}
+              >
+                {category} ({count})
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </section>
+    </>
+  );
 }
--- src/components/post_list/PostGrid.tsx
+++ src/components/post_list/PostGrid.tsx
@@ -1,37 +1,37 @@
 import {
-	getAllPostCount,
-	getCategoryList,
-	getSortedPostList,
-} from "@/service/posts";
-import PostCard from "./PostCard";
-import CategoryList from "./CategoryList";
+  getAllPostCount,
+  getCategoryList,
+  getSortedPostList,
+} from '@/service/posts';
+import PostCard from './PostCard';
+import CategoryList from './CategoryList';
 
 type PostListProps = {
-	category?: string;
+  category?: string;
 };
 
 export default async function PostGrid({ category }: PostListProps) {
-	const postList = await getSortedPostList(category);
-	const categoryList = await getCategoryList();
-	const allPostCount = await getAllPostCount();
+  const postList = await getSortedPostList(category);
+  const categoryList = await getCategoryList();
+  const allPostCount = await getAllPostCount();
 
-	return (
-		<section className='mx-auto mt-12 w-full max-w-[950px] px-4'>
-			<h1 className='text-4xl font-bold mb-3 text-blue-900 dark:text-blue-300'>
-				{category ?? "All"}
-			</h1>
-			<CategoryList
-				allPostCount={allPostCount}
-				categoryList={categoryList}
-				selectedCategory={category}
-			/>
-			<section>
-				<ul className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:gap-12'>
-					{postList.map((post) => (
-						<PostCard key={post.url + post.date} post={post} />
-					))}
-				</ul>
-			</section>
-		</section>
-	);
+  return (
+    <section className='mx-auto mt-12 w-full max-w-[950px] px-4'>
+      <CategoryList
+        allPostCount={allPostCount}
+        categoryList={categoryList}
+        selectedCategory={category}
+      />
+      <section>
+        <ul className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:gap-12'>
+          {postList.map((post) => (
+            <PostCard
+              key={post.url + post.date}
+              post={post}
+            />
+          ))}
+        </ul>
+      </section>
+    </section>
+  );
 }
